6290591acf5182e512e8fcf7397807b8cee9ccf2
Maulik Paddhariya <mpaddhariya@tbkcreative.com>
2023-05-04 11:14:48 +0530

    Loading scss file and import default block classe in component

--- wp-content/plugins/firstblock/build/index.asset.php
+++ wp-content/plugins/firstblock/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blocks', 'wp-element'), 'version' => '6292b44925b4a59787b4');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element'), 'version' => '3c81f048132bc8973528');
