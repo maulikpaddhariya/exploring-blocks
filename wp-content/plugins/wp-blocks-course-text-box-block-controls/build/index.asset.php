02391c0785380ed1d69f20580d3b2b3ecffe8bb9
Maulik Paddhariya <mpaddhariya@tbkcreative.com>
2023-05-24 11:29:03 +0530

    Adding Options in the Block Toolbar Using BlockControl

---
+++ wp-content/plugins/wp-blocks-course-text-box-block-controls/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-polyfill'), 'version' => '7d9834f03dbe1d026afa8688f39857de');
